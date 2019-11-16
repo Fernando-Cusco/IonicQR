13f88a01160091780f2e197f93363483fc1cca9b
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-11-15 19:02:53 -0500

    [add] ciclo de vida de un componente

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  //cuando la vista es cargada se dispara
+  ionViewDidEnter() {
+    console.log('did Enter');
+  }
+  //cuando se cambia de vista o componente
+  ionViewDidLeave() {
+    console.log('Cuando ya salio del punto de vista del usuario');
+  }
+  //cuando la vista va a cargar se dispara
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter');
+  }
+  //cuando se va a cerra la vista se dispara
+  ionViewWillLeave() {
+    console.log('Cuando ya se va a cerrar la vista');
+  }
+
   constructor() {}
 
 }
