2e2dd4b88129ad215542b4498edc08980275858b
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-11-15 21:42:12 -0500

    [add] almacenamos el historial en el storage y la recuperamos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { IonicStorageModule } from '@ionic/storage';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
   providers: [
     StatusBar,
     BarcodeScanner,
--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Registro } from '../models/registro.model';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,21 @@ export class DataLocalService {
 
   historial: Registro[] = [];
 
-  constructor() { }
+  constructor(private storage: Storage) { 
+    this.cargarHistorial();
+  }
 
   guardarRegistro(format: string, text: string) {
     const nuevo = new Registro(format, text);
     this.historial.unshift(nuevo);
+    this.storage.set('registros', this.historial);
     console.log('Datos ', this.historial);
-    
+  }
+
+  async cargarHistorial() {
+    const datos = await this.storage.get('registros');
+    this.historial = datos || [];
+
   }
 
 }
